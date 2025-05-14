bf5390246a7efe90467961ce06807f91c36e5bb7
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2025-05-14 15:19:34 +0530

    Add files via upload

---
+++ Login pages/mod.js
@@ -0,0 +1,66 @@
+/*
+const loginform = document.querySelector(".login-form");
+const registerform = document.querySelector(".register-form");
+const logintitle = document.querySelector(".log-title");
+const registertitle = document.querySelector(".reg-title");
+const center = document.querySelector(".center");
+const signinbtn = document.querySelector("#signinbtn");
+const signupbtn = document.querySelector("#signinbtn");
+
+function loginfunction(){
+    loginform.style.left="50%";
+    loginform.style.opacity=1;
+    registerform.style.left="150%";
+    registerform.style.opacity=0;
+    center.style.height="500px";
+    logintitle.style.top="50%";
+    logintitle.style.opacity=1;
+    registertitle.style.top="50px";
+    registertitle.style.opacity=0;
+}
+function registerfunction(){
+    loginform.style.left="-50%";
+    loginform.style.opacity=0;
+    registerform.style.left="50%";
+    registerform.style.opacity=1;
+    center.style.height="560px";
+    logintitle.style.top="-60px";
+    logintitle.style.opacity=0;
+    registertitle.style.top="50%";
+    registertitle.style.opacity=1;
+}
+    */
+
+
+
+const loginform = document.querySelector(".login-form");
+const registerform = document.querySelector(".register-form");
+const logintitle = document.querySelector(".log-title");
+const registertitle = document.querySelector(".reg-title");
+const center = document.querySelector(".center");
+const signinbtn = document.querySelector(".signinbtn");
+const signupbtn = document.querySelector(".signinbtn");
+
+function loginfunction(){
+    loginform.style.left="50%";
+    loginform.style.opacity=1;
+    registerform.style.left="150%";
+    registerform.style.opacity=0;
+    center.style.height="500px";
+    logintitle.style.top="50%";
+    logintitle.style.opacity=1;
+    registertitle.style.top="50px";
+    registertitle.style.opacity=0;
+}
+
+function registerfunction(){
+    loginform.style.left="-50%";
+    loginform.style.opacity=0;
+    registerform.style.left="50%";
+    registerform.style.opacity=1;
+    center.style.height="560px";
+    logintitle.style.top="-60px";
+    logintitle.style.opacity=0;
+    registertitle.style.top="50%";
+    registertitle.style.opacity=1;
+}
